0d322075a6ad95816b8fec82899e6d792ee8602a
shubhamwhj <shubham.verma@whitehatjr.com>
2021-07-22 11:11:03 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,46 @@
+
+var player,player2;
+var platform,platform2;
+var player_image, platform_image;
+
+function preload(){
+  player_image=loadImage("images/jack2.png")
+  platform_image = loadImage("images/p1.png");
+  player2_image=loadImage("images/jack1.png")
+}
+
+function setup() {
+  createCanvas(500, 500);
+  player = createSprite(250,300,20,50);
+  player.addImage("player",player_image)
+
+  platform = createSprite(250,350,100,10);
+  platform.addImage("platform", platform_image)
+  platform.scale=0.15
+
+  player2 = createSprite(350,300,20,50);
+  platform2 = createSprite(350,350,100,10);
+  
+  player2.addImage("player",player2_image)
+  platform2.addImage("platform", platform_image)
+  platform2.scale=0.15
+  
+}
+
+function draw() {
+  background('#F2F5B6');
+  
+  if(keyDown("up"))
+  {
+    player.velocityY=-10
+  }
+
+  drawSprites();
+}
+
+
+
+
+
+
+
