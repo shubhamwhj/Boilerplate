3e611f180490f9fdb239c44cc3e526da3666382e
shubhamwhj <shubham.verma@whitehatjr.com>
2021-07-22 11:28:48 +0530

    boilerplate

--- sketch.js
+++ sketch.js
@@ -1,20 +1,10 @@
 
-var player;
-var platform;
-var player_image, platform_image;
-
 function preload(){
-  player_image=loadImage("images/jack2.png")
-  platform_image = loadImage("images/p1.png");
 }
 
 function setup() {
   createCanvas(500, 500);
-  player = createSprite(250,300,20,50);
-
-  platform = createSprite(250,350,100,10);
-  platform.addImage("platform", platform_image)
-  platform.scale=0.15
+ 
 
 }
 
